5ae1882c8266ebaf5a3bb469a1d90dbd11dbda6f
kirill <kirill.tsyganov@internet.ru>
2025-12-24 22:21:44 +0300

    action to page for log in and log up was added

---
+++ WebApp/Controllers/LogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+	public class LogController : Controller
+	{
+		// GET: LogController
+		public ActionResult AuthorizationPage()
+		{
+			return View();
+		}
+		public ActionResult RegistrationPage()
+		{
+			return View();
+		}
+	}
+}
